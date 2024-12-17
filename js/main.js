2c24249252e97c993ee85466ee22b7193b242962
Nel Rondón <nelucho.nel@gmail.com>
2024-12-16 22:23:40 -0400

    add modal to show pokemon full stats

--- js/main.js
+++ js/main.js
@@ -35,18 +35,18 @@ const renderPokemons = (pokemons) => {
 
   pokemons.map((pokemon) => {
     const item = `
-      <li class="pokemon">
+      <li class="pokemon" id="${pokemon.id}">
               <img src="https://img.pokemondb.net/artwork/large/${pokemon.name.toLowerCase()}.jpg" alt="">
               <div>
                   <h3>
-                      <span>00${pokemon.id}</span>
+                      <span class="pk-id">00${pokemon.id}</span>
                       ${pokemon.name}
                   </h3>
                   <h4>Tipos:</h4>
                   <ul>
                       ${pokemon.type.map(
                         (type) =>
-                          `<li class="${type.toLowerCase()}">${type.toUpperCase()}</li>`
+                          `<li class="type ${type.toLowerCase()}">${type.toUpperCase()}</li>`
                       )}
                   </ul>
               </div>  
@@ -75,8 +75,6 @@ formFilter.addEventListener("change", (e) => {
   const valueInput = formFilter.filterName.value.toLowerCase();
   const valueSelect = formFilter.filterType.value;
 
-  console.log(valueSelect);
-
   // FILTRAMOS POR NOMBRE
   const pokemonsFiltered = allPokemons.filter((pokemon) => {
     return (
@@ -86,4 +84,73 @@ formFilter.addEventListener("change", (e) => {
   });
 
   renderPokemons(pokemonsFiltered);
+});
+
+const modal = document.getElementById("modal");
+
+const idTag = document.getElementById("info-id");
+const nameTag = document.getElementById("info-name");
+const imgTag = document.getElementById("info-img");
+const typeTag = document.getElementById("info-types");
+const weakTag = document.getElementById("info-w-types");
+const resiTag = document.getElementById("info-r-types");
+
+const hpSpan = document.getElementById("sp-hp");
+const attSpan = document.getElementById("sp-att");
+const defSpan = document.getElementById("sp-def");
+const sattSpan = document.getElementById("sp-satt");
+const sdefSpan = document.getElementById("sp-sdef");
+const spdSpan = document.getElementById("sp-spd");
+
+listPokemons.addEventListener("click", (e) => {
+  if (e.target != listPokemons) {
+    const liPokemon = e.target.closest(".pokemon");
+    const id = liPokemon.id;
+
+    const { name, type, stats, weaknesses, resistances } = allPokemons.find(
+      (pk) => pk.id == id
+    );
+
+    idTag.textContent = `00${id}`;
+    nameTag.textContent = name;
+    imgTag.setAttribute(
+      "src",
+      `https://img.pokemondb.net/artwork/large/${name.toLowerCase()}.jpg`
+    );
+
+    const { hp, attack, defense, specialAttack, specialDefense, speed } = stats;
+
+    [
+      [typeTag, type],
+      [weakTag, weaknesses],
+      [resiTag, resistances],
+    ].forEach((lst) => {
+      const [tag, value] = lst;
+      tag.textContent = "";
+      value.forEach((type) => {
+        const item = `
+          <li class="type ${type.toLowerCase()}">${type.toUpperCase()}</li>
+        `;
+        tag.insertAdjacentHTML("beforeend", item);
+      });
+    });
+
+    [
+      [hpSpan, hp],
+      [attSpan, attack],
+      [defSpan, defense],
+      [sattSpan, specialAttack],
+      [sdefSpan, specialDefense],
+      [spdSpan, speed],
+    ].forEach(([tag, value]) => {
+      tag.textContent = value;
+    });
+
+    modal.classList = "pokemon-attrs active";
+  }
+});
+
+const iconClose = document.getElementById("icon-close");
+iconClose.addEventListener("click", () => {
+  modal.classList = "pokemon-attrs";
 });
